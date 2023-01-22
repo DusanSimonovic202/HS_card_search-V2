1c830dc94f95b832ff5a6737065834daa5262deb
Dusancar123321 <Dusancar123321@gmail.com>
2023-01-22 01:08:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect, useRef } from "react";
+import Card from "./Card";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [hsdata, setHsData] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${search}`,
+      headers: {
+        "X-RapidAPI-Key": "bcbdf03864msh34ccf7b9c42eda4p12d748jsn062fb40a2698",
+        "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setHsData(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, [search]);
+  console.log(hsdata);
+
+  const inputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    setSearch(inputRef.current.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search for any card</h1>
+      <input type="text" placeholder="Search..." ref={inputRef} />
+      <button className="submitbtn" onClick={handleButtonClick}>
+        Submit
+      </button>
+      {hsdata.map((card) => (
+        <div key={card.cardId}>
+          <Card
+            key={card.Id}
+            name={card.name}
+            img={card.img}
+            cardSet={card.cardSet}
+            faction={card.faction}
+            playerClass={card.playerClass}
+            type={card.type}
+            race={card.race}
+          />
+        </div>
+      ))}
     </div>
   );
 }
