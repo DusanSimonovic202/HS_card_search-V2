71912b8e27ceaf4b404d4c50f593c079cd31b540
Dusancar123321 <Dusancar123321@gmail.com>
2023-01-22 17:15:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import "./App.css";
-import axios from "axios";
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Card from "./Card";
+import axios from "axios";
+import "./App.css";
+import LoadingDots from "./style/LoadingDots";
 
 function App() {
   const [search, setSearch] = useState("");
   const [hsdata, setHsData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const options = {
-      method: "GET",
-      url: `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${search}`,
-      headers: {
-        "X-RapidAPI-Key": "bcbdf03864msh34ccf7b9c42eda4p12d748jsn062fb40a2698",
-        "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
-      },
-    };
-
     axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-        setHsData(response.data);
+      .get(
+        `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/${search}`,
+        {
+          headers: {
+            "X-RapidAPI-Key":
+              "bcbdf03864msh34ccf7b9c42eda4p12d748jsn062fb40a2698",
+            "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+          },
+        }
+      )
+      .then(({ data }) => {
+        setHsData(data);
+        setLoading(false);
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.error(error);
       });
   }, [search]);
-  console.log(hsdata);
 
   const inputRef = useRef(null);
 
@@ -36,26 +37,40 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Search for any card</h1>
+    <div className="App" style={loading ? { marginTop: "200px" } : {}}>
+      <h1>Welcome to Hearthstone Card database</h1>
+      <h2>You can search here for any card</h2>
+
       <input type="text" placeholder="Search..." ref={inputRef} />
       <button className="submitbtn" onClick={handleButtonClick}>
         Submit
       </button>
-      {hsdata.map((card) => (
-        <div key={card.cardId}>
-          <Card
-            key={card.Id}
-            name={card.name}
-            img={card.img}
-            cardSet={card.cardSet}
-            faction={card.faction}
-            playerClass={card.playerClass}
-            type={card.type}
-            race={card.race}
-          />
+
+      {loading ? (
+        <div className="divsearch">
+          <h4> We are waing for your search</h4> <LoadingDots />
         </div>
-      ))}
+      ) : (
+        <div>
+          <h3>This is what we found</h3>
+          <LoadingDots />
+          {hsdata.map((card, i) => (
+            <div key={i}>
+              <Card
+                key={i}
+                rarity={card.rarity}
+                name={card.name}
+                img={card.img}
+                cardSet={card.cardSet}
+                faction={card.faction}
+                playerClass={card.playerClass}
+                type={card.type}
+                race={card.race}
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,15 @@
 import React from "react";
 
-const Card = ({ name, img, cardSet, faction, playerClass, type, race }) => {
+const Card = ({
+  name,
+  img,
+  cardSet,
+  faction,
+  playerClass,
+  type,
+  race,
+  rarity,
+}) => {
   return (
     <div className="card">
       {cardSet ? <h3>Card set: {cardSet}</h3> : ""}
@@ -8,6 +17,7 @@ const Card = ({ name, img, cardSet, faction, playerClass, type, race }) => {
       {img ? <img src={img} alt={name} /> : ""}
       {faction ? <h4>Faction: {faction}</h4> : ""}
       {playerClass ? <h4> Class: {playerClass}</h4> : ""}
+      {rarity ? <h4> rarity: {rarity}</h4> : ""}
       {type ? <h4>Type: {type}</h4> : ""}
       {race ? <h4>Race: {race}</h4> : ""}
     </div>
